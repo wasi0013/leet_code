7aa52ac839f05a340d1b1faf92103892d06297a5
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2022-06-24 06:12:14 +0600

    refactored 2264

--- problems/2264_largest_3_same_digit_number_in_string.py
+++ problems/2264_largest_3_same_digit_number_in_string.py
@@ -1,5 +1,13 @@
 from itertools import groupby
 class Solution:
     def largestGoodInteger(self, num: str) -> str:
-        ans = max([c for key, group in groupby(num) if len(c:=''.join(group)[:3]) >= 3], default='')
-        return "000" if ans =="0" else ans
+        good_nums = [] 
+        
+        for _k, group in groupby(num): 
+            s = ''.join(group)[:3]
+            if len(s) == 3:
+                good_nums.append(s)
+                
+        ans = str(max(good_nums, default=''))
+        
+        return "000" if ans == "0" else ans
